1888bfbc3eee13b905c8e272e05d6b3a971cff91
Sasaya <storyn26383@gmail.com>
2023-10-13 16:52:44 +0800

    fzf config

--- zsh/config/fzf.zsh
+++ zsh/config/fzf.zsh
@@ -1,2 +1,3 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND="ag --hidden --skip-vcs-ignores --ignore=.git -g ''"
+export FZF_DEFAULT_OPTS='--preview-window hidden,right,50%,border-left,\<70\(hidden,up,50%,border-down\) --bind ctrl-/:toggle-preview'
