9376f23a4deeafd6def8b2ef821615bfb6a02532
Sasaya <storyn26383@gmail.com>
2024-05-22 00:31:12 +0800

    use oh my posh

--- zsh/config/powerline.zsh
+++ zsh/config/powerline.zsh
@@ -1,1 +1,18 @@
-source /opt/homebrew/lib/python3.11/site-packages/powerline/bindings/zsh/powerline.zsh
+export POWERLINE_PATH=/opt/homebrew/lib/python3.11/site-packages/powerline/bindings
+export POWERLINE_CONFIG_PATHS=$POWERLINE_PATH/config_files
+
+_powerline_command=$POWERLINE_COMMAND
+eval "$(oh-my-posh init zsh --config $DOTFILES/oh-my-posh/themes/zsh.omp.json)"
+export POWERLINE_COMMAND=$_powerline_command
+
+# hook
+function set_poshcontext() {
+  export POSH_JOBNUM=${(%):-%j}
+  export POSH_VI_KEYMAP=${VI_KEYMAP}
+}
+
+# hook
+function after_keymap_changed() {
+  prompt_ohmyposh_precmd
+  zle reset-prompt
+}
