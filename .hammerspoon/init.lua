3dfd2c87937c475b5982726abf538764206653b5
Sasaya <storyn26383@gmail.com>
2025-06-23 23:36:17 +0800

    feat: hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,3 @@
--- hs.eventtap.keyStroke(modifiers, character[, delay])
--- hs.hotkey.bind(mods, key, message, pressedfn, releasedfn, repeatfn)
-
 local alertStyle = {
   textStyle = {
     paragraphStyle = {
@@ -9,24 +6,27 @@ local alertStyle = {
   }
 }
 
-local function bind(from, to, pressedfn, releasedfn)
-  local fn = function ()
-    if pressedfn ~= nil then pressedfn() end
+local function bind(from, to, pressed, released)
+  local pressedfn = function()
+    if pressed ~= nil then pressed() end
     if to ~= nil then hs.eventtap.keyStroke(to.mods or {}, to.key, 0) end
   end
+  local releasedfn = released or nil
+  local repeatfn = pressedfn
 
-  return hs.hotkey.bind(from.mods or {}, from.key, fn, releasedfn, fn)
+  return hs.hotkey.bind(from.mods or {}, from.key, pressedfn, releasedfn, repeatfn)
 end
 
 -- global
-bind({ key = 'rightctrl' }, { key = 'fn' })
+-- bind({ key = 'rightctrl' }, { key = 'fn' })
+-- bind({ mods = {'cmd'}, key = 'tab' }, { key = 'f13' }):enable()
 
 -- local disableBackspace = bind({ key = 'delete' }, nil, function ()
 --   hs.alert.show('BACKSPACE KEY IS FORBIDDEN', alertStyle)
 -- end)
 
 -- disable when a specific application is active
-local keys = {
+local key_bindings = {
   -- bind(
   --   { mods = {'ctrl'}, key = 'h' },
   --   { key = 'delete' },
@@ -35,24 +35,24 @@ local keys = {
   -- ),
   bind({ mods = {'ctrl'}, key = 'h' }, { key = 'delete' }),
   bind({ mods = {'ctrl'}, key = '[' }, { key = 'escape' }),
-  -- bind({ mods = {'ctrl'}, key = 'p' }, { key = 'up' }),
-  -- bind({ mods = {'ctrl'}, key = 'n' }, { key = 'down' }),
-  -- bind({ mods = {'ctrl'}, key = 'b' }, { key = 'left' }),
-  -- bind({ mods = {'ctrl'}, key = 'f' }, { key = 'right' }),
-  bind({ mods = {'ctrl'}, key = 'e' }, { mods = {'command'}, key = 'right' }),
-  bind({ mods = {'ctrl'}, key = 'a' }, { mods = {'command'}, key = 'left' })
+  bind({ mods = {'ctrl'}, key = 'p' }, { key = 'up' }),
+  bind({ mods = {'ctrl'}, key = 'n' }, { key = 'down' }),
+  bind({ mods = {'ctrl'}, key = 'b' }, { key = 'left' }),
+  bind({ mods = {'ctrl'}, key = 'f' }, { key = 'right' }),
+  bind({ mods = {'ctrl'}, key = 'e' }, { mods = {'cmd'}, key = 'right' }),
+  bind({ mods = {'ctrl'}, key = 'a' }, { mods = {'cmd'}, key = 'left' }),
 }
 
-hs.application.watcher.new(function (name, event, app)
+hs.application.watcher.new(function (app, event)
   if event == hs.application.watcher.activated then
-    local method
+    local method = 'enable'
 
-    if name == 'iTerm2' or name == 'MacVim' then method = 'disable'
-    else method = 'enable'
+    if app == 'Alacritty' or app == 'iTerm2' then
+      method = 'disable'
     end
 
-    for i, key in ipairs(keys) do
-      key[method](key)
+    for _, binding in ipairs(key_bindings) do
+      binding[method](binding)
     end
   end
 end):start()
