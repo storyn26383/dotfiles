44b469d2df4d8a85bc85f544e251765e3d7f078e
Sasaya <storyn26383@gmail.com>
2025-07-12 23:05:57 +0800

    feat: modify fzf config

--- zsh/config/fzf.zsh
+++ zsh/config/fzf.zsh
@@ -1,3 +1,6 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [ -x "$(command -v fzf)" ]; then
+  source <(fzf --zsh)
+fi
+
 export FZF_DEFAULT_COMMAND="rg --hidden --no-ignore-vcs --glob '!.git/*' --files"
 export FZF_DEFAULT_OPTS='--preview-window hidden,right,50%,border-left,\<70\(hidden,up,50%,border-down\) --bind ctrl-/:toggle-preview'
