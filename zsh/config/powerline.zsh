a8efb3edbcb367449716c9aad1133e12bc6c0b5c
Sasaya <storyn26383@gmail.com>
2025-07-12 23:14:24 +0800

    feat: for mac mini

--- zsh/config/powerline.zsh
+++ zsh/config/powerline.zsh
@@ -1,4 +1,4 @@
-export POWERLINE_PATH=/opt/homebrew/lib/python3.11/site-packages/powerline
+export POWERLINE_PATH=/opt/homebrew/lib/python3.13/site-packages/powerline
 export POWERLINE_CONFIG_PATHS=$POWERLINE_PATH/config_files
 
 _powerline_command=$POWERLINE_COMMAND
