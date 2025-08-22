1b1d4ba200cd5a0592fbc742aec1c2f79301e592
Sasaya <storyn26383@gmail.com>
2025-08-22 21:57:35 +0800

    feat: add server script

---
+++ dot_oh-my-zsh/custom/scripts/server.zsh
@@ -0,0 +1,29 @@
+_server_cube () {
+  mosh cube -- tmux new -ADs cube
+}
+
+_server_mini () {
+  mosh cube -- ssh mini -t 'tmux new -ADs Sasaya'
+}
+
+server () {
+  case $1 in
+    cube)
+      _server_cube
+      ;;
+
+    mini)
+      _server_mini
+      ;;
+
+    *)
+      echo 'Unknow server:' $1
+      ;;
+  esac
+}
+
+_servers_completion () {
+  _shared_generate_completion 'cube mini'
+}
+
+complete -F _servers_completion server
